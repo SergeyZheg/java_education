33ca912f33b600aefeab6c9cea8661faff200158
SergeyZheg <serzeg@mail.ru>
2021-07-09 18:30:26 +0300

    Создан класс MySecondProgram для Задания №2

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
@@ -0,0 +1,22 @@
+package ru.stqa.pft.sandbox;
+
+public class MySecondProgram {
+
+    public static void main(String[] args) {
+
+        //       Point p1 = new Point(5,10);
+        //       Point p2 = new Point(20, 20);
+
+        //       System.out.println("Расстояние между точками p1 и p2 = " + distance(p1,p2));
+
+        Point p = new Point(5, 10, 20, 20);
+        System.out.println("Расстояние между точками p1 и p2 = " + p.distance());
+
+    }
+
+    //  public static double distance(Point p1, Point p2) {
+
+    // return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    // return Math.sqrt(Math.pow((p2.x - p1.x),2) + Math.pow((p2.y - p1.y),2));
+    //   }
+}
