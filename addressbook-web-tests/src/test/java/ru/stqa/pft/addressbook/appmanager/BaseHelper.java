e5bea24b319b50168496d325e97c6dee27c3a781
SergeyZheg <serzeg@mail.ru>
2021-07-23 11:13:22 +0300

    Добавлена возможность работать с несколькими браузерами и оптимизация полей ввода

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/BaseHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/BaseHelper.java
@@ -1,9 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 
 public class BaseHelper {
     protected WebDriver wd;
@@ -18,8 +15,13 @@ public class BaseHelper {
 
     protected void type(By locator, String text) {
         click(locator);
-        wd.findElement(locator).clear();
-        wd.findElement(locator).sendKeys(text);
+        if (text != null) {
+            String existingText = wd.findElement(locator).getAttribute("value");
+            if (! text.equals(existingText)) {
+                wd.findElement(locator).clear();
+                wd.findElement(locator).sendKeys(text);
+            }
+        }
     }
 
 
